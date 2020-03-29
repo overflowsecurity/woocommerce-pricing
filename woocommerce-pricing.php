baae6ade51683f6a7660543638e11147f0c96c74
jtharpe <jtharpe@overflownetworks.com>
2020-03-29 00:01:34 -0400

    Updated Title

--- woocommerce-pricing.php
+++ woocommerce-pricing.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Plugin Name: Woocommerce Pricing
+    Plugin Name: Woocommerce Shipping Pricing
     description: This plugin makes it possible to price shipping by number of items.
     Author: Justin Tharpe
     Version: Beta 1.0.0
