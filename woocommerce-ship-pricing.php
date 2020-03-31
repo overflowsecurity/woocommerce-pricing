6f38d45fd5271597ea2a68033d1626cadc64e976
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-03-30 21:54:33 -0400

    Updated Comments

--- woocommerce-ship-pricing.php
+++ woocommerce-ship-pricing.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Plugin Name: Woocommerce Shipping Pricing
+    Plugin Name: Woocommerce Shipping by Item Pricing
     description: This plugin makes it possible to price shipping by number of items.
     Author: Justin Tharpe
     Version: Beta 1.0.0
